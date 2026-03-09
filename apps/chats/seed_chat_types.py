0b80c0eef89f4a3bbfbabbdec1e088ed6b5d15e1
sillvallsi2000-bit <sillvallsi2000@gmail.com>
2026-03-09 08:59:55 +0100

    Update chat types model and enum usage

--- apps/chats/seed_chat_types.py
+++ apps/chats/seed_chat_types.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand
-from apps.chat.models import ChatTypeModel
+from apps.chat.models import ChatTypesModel
+from core.enum.enum import ChatTypesChoice
+from 
 
 class Command(BaseCommand):
   def handle(self,*args, **kwargs ):
-    types =["DIRECT", "GROUP", "CHANNEL"]
+    types = [ChatTypesChoice.DIRECT, ChatTypesChoice.GROUP, ChatTypesChoice.CHANNEL]
     for t in types:
-      ChatTypeModel.objects.get_or_create(name = t)
+      ChatTypesModel.objects.get_or_create(name = t)
     self.stdout.write(self.style.SUCCESS("Chat types created successfully"))
   
 
