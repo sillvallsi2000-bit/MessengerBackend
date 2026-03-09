98c1a9d89a05a85a355ac5d9f06463c7665db97a
sillvallsi2000-bit <sillvallsi2000@gmail.com>
2026-03-09 09:01:30 +0100

    Remove seed_chat_types.py management command

--- apps/chats/seed_chat_types.py
+++ apps/chats/seed_chat_types.py
@@ -1,13 +1,1 @@
-from django.core.management.base import BaseCommand
-from apps.chat.models import ChatTypesModel
-from core.enum.enum import ChatTypesChoice
-from 
-
-class Command(BaseCommand):
-  def handle(self,*args, **kwargs ):
-    types = [ChatTypesChoice.DIRECT, ChatTypesChoice.GROUP, ChatTypesChoice.CHANNEL]
-    for t in types:
-      ChatTypesModel.objects.get_or_create(name = t)
-    self.stdout.write(self.style.SUCCESS("Chat types created successfully"))
-  
 
