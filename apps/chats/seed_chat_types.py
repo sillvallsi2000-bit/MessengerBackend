5312677547aff0383a01dec2271ff07aa880ca68
sillvallsi2000-bit <sillvallsi2000@gmail.com>
2026-03-09 08:52:57 +0100

    Update seed_chat_types.py

--- apps/chats/seed_chat_types.py
+++ apps/chats/seed_chat_types.py
@@ -1,1 +1,11 @@
+from django.core.management.base import BaseCommand
+from apps.chat.models import ChatTypeModel
+
+class Command(BaseCommand):
+  def handle(self,*args, **kwargs ):
+    types =["DIRECT", "GROUP", "CHANNEL"]
+    for t in types:
+      ChatTypeModel.objects.get_or_create(name = t)
+    self.stdout.write(self.style.SUCCESS("Chat types created successfully"))
+  
 
